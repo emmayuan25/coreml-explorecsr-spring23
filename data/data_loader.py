885c1ed5efa3356f070ea436db7bf09c36dde044
Emma Yuan <jinyueyu@usc.edu>
2023-04-14 11:32:48 -0700

    add autoencoder

--- data/data_loader.py
+++ data/data_loader.py
@@ -23,9 +23,9 @@ def load_data():
     processed_data_file['official_video'] = processed_data_file['official_video'].replace(np.nan, 0)
     processed_data_file['official_video'] = processed_data_file['official_video'].astype(int)
 
-    filtered_data = processed_data_file[(processed_data_file['Stream'] >= 50000000.0)]
-
-    print(filtered_data)
+    # split data by cutoff
+    stream_cutoff = 50000000.0
+    filtered_data = processed_data_file[(processed_data_file['Stream'] >= stream_cutoff)]
 
     # split data set based on criteria of popular songs
     training_labels = filtered_data[['TrackID', 'Views', 'Likes', 'Comments', 'Stream']]
@@ -34,13 +34,9 @@ def load_data():
     testing_labels = testing_inputs.loc[:, ['TrackID', 'Views', 'Likes', 'Comments', 'Stream']]
     testing_inputs = testing_inputs.drop(columns=['TrackID', 'Views', 'Likes', 'Comments', 'Stream'])
 
-    return training_labels, training_inputs, testing_labels, testing_inputs
-
-
-def prediction(training_labels, training_inputs, testing_labels, testing_inputs):
-    # normalize columns relating to the characteristics of the songs
+    # normalize columns relating to the characteristics of the songs on training data
     key_min = training_inputs['Key'].min()
-    key_max = training_labels['Key'].max()
+    key_max = training_inputs['Key'].max()
     training_inputs['Key'] = (training_inputs['Key'] - key_min) / (key_max - key_min)
 
     loudness_min = training_inputs['Loudness'].min()
@@ -54,6 +50,33 @@ def prediction(training_labels, training_inputs, testing_labels, testing_inputs)
     duration_min = training_inputs['Duration_ms'].min()
     duration_max = training_inputs['Duration_ms'].max()
     training_inputs['Duration_ms'] = (training_inputs['Duration_ms'] - duration_min) / (duration_max - duration_min)
+
+    print("------------------------------")
+    print("Training set has " + str(len(training_labels)) + " entries")
+    print("Testing set has " + str(len(testing_labels)) + " entries")
+    print("Popular streaming cutoff: " + str(stream_cutoff))
+    print("------------------------------")
+    print("Number of input columns: " + str(len(training_inputs.columns)))
+    print(training_inputs.columns)
+    print("------------------------------")
+    print("Label columns: " + str(len(training_labels.columns)))
+    print(training_labels.columns)
+    print("------------------------------")
+    print("Training input example")
+    print(training_inputs.iloc[0])
+    print("------------------------------")
+    print("Training label example")
+    print(training_labels.iloc[0])
+    print("------------------------------")
+    print("Testing inputs example")
+    print(testing_inputs.iloc[0])
+    print("------------------------------")
+    print("Testing labels example")
+    print(testing_labels.iloc[0])
+
+
+    return training_labels, training_inputs, testing_labels, testing_inputs
+
 
 
 if __name__ == "__main__":
